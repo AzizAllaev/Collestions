108863839ab951ea9197206633ef5a6317c837f8
azizallaev78@gmail.com <azizallaev78@gmail.com>
2025-01-26 17:47:26 +0500

    ListOfTCOllection remake

--- CollectionSample/Program.cs
+++ CollectionSample/Program.cs
@@ -9,34 +9,72 @@ namespace CollectionSample
 		/// работа основных методов CRUD.
 		/// </summary>
 		/// <param name="args"></param>
-		static void Main(string[] args)
+
+
+		// Create
+		static void Create(List<Person> person)
 		{
-			// Create
-			var person = new List<Person>();
-			person.Add(new Person { Name = "Tom", Age = 23 });
-			person.Add(new Person { Name = "Alice", Age = 29 });
-			person.Add(new Person { Name = "Bob", Age = 34 });
-			person.Add(new Person { Name = "Sam", Age = 45 });
+			Console.WriteLine("Создание коллекции!");
+			// Создаётся два листа для демонстрирования какими способами можно создать коллекцию
+			var person2 = new List<Person>
+			{
+				new Person { Name = "Tom", Age = 23 },
+				new Person { Name = "Alice", Age = 29 },
+				new Person { Name = "Sam", Age = 45 }
+			};
 
-            // Read
-            foreach (Person person1 in person)
-            {
-				Console.WriteLine($"Имя: {person1.Name} || Возраст: {person1.Age}");
-            }
+			person.Add(new Person { Name = "Sanya", Age = 33 });
+			person.Add(new Person { Name = "Aziz", Age = 65 });
+			person.Add(new Person { Name = "Tadd", Age = 22 });
+		}
 
-			// Update
-			int index = person.BinarySearch(new Person { Name = "Anton", Age = 60 });
-			person.RemoveAt(index);
-			person.Reverse(index, 1);
-			foreach(Person person1 in person)
+		// Read
+		static void ReadAll(List<Person> person)
+		{
+			Console.WriteLine("Чтение коллекции:");
+			foreach (Person person1 in person)
 			{
 				Console.WriteLine($"Имя: {person1.Name} || Возраст: {person1.Age}");
 			}
+		}
 
-			// Delete
-			person.RemoveRange(1, 1);
+		// Read
+		static void ReadOne(List<Person> person, string PName)
+		{
+			Console.WriteLine("Чтение одного элемента коллекции:");
+			int index = person.FindIndex(p => p.Name == PName);
+			Console.WriteLine($"Имя: {person[index].Name} || Возраст: {person[index].Age}");
+		}
+
+		// Update
+		static void Update(List<Person> person)
+		{
+			Console.WriteLine("Обновление коллекции:");
+			person[0] = new Person { Name = "Anton", Age = 60 };
+			person.Reverse();
+			person.ForEach(p => Console.WriteLine($"Имя: {p.Name} || Возраст: {p.Age}"));
+		}
+
+		// Delete
+		static void Delete(List<Person> person)
+		{
+			Console.WriteLine("Удаление коллекции!");
+			person.RemoveAt(1);
 			person.RemoveAll(p => p.Age > 30);
 			person.Clear();
+		}
+
+		static void Main(string[] args)
+		{
+			// CRUD
+			List<Person> person = new List<Person>();
+			Create(person);
+			ReadAll(person);
+			ReadOne(person, "Aziz");
+			Update(person);
+			Delete(person);
+
+			Console.ReadLine();
 		}
 	}
 }
